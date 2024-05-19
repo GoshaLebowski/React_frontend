f6335403d317d639e34f9ac3ac6cefdbd1152d91
GoshaLebowski <sirinkinegor@gmail.com>
2024-05-19 15:52:11 +0500

    Add validation to login form - Add validation with react-hook-form - Add yup - Refactoring login adn Register component

--- frontend/src/common/errors/index.ts
+++ frontend/src/common/errors/index.ts
@@ -1,3 +1,7 @@
 export const AppErrors = {
-    'PasswordDoNotMatch': 'Пароли не совпадают',
+    PasswordDoNotMatch: 'У вас не совпадают пароли',
+    InvalidEmail: 'Введите корректный email',
+    RequiredField: 'Это поле обязательное',
+    minLength: 'Минимальная длина 4 символа',
+    InvalidPassword: 'Пароль должен содержать спецсимвол, однин заглавный символ, одну цифру'
 }
--- frontend/src/components/auth/register/index.tsx
+++ frontend/src/components/auth/register/index.tsx
@@ -28,50 +28,60 @@ const RegisterPage: React.FC<IPropsRegister> = (props: IPropsRegister): JSX.Elem
                 Введите данные для регистрации
             </Typography>
             <TextField
+                error={!!errors.firstName}
                 fullWidth={true}
                 margin={'normal'}
                 label="Имя"
                 variant="outlined"
                 placeholder={'Введите ваш имя'}
-                {...register('firstName', {
-                    required: 'Это обязательное поле',
-                })}
+                helperText={errors.firstName ? `${errors.firstName.message}` : ''}
+                {...register('firstName')}
             />
             <TextField
+                error={!!errors.username}
                 fullWidth={true}
                 margin={'normal'}
                 label="Username"
                 variant="outlined"
                 placeholder={'Введите ваш username'}
-                {...register('username', {
-                    required: 'Это обязательное поле',
-                })}
+                helperText={errors.username ? `${errors.username.message}` : ''}
+                {...register('username')}
             />
             <TextField
+                error={!!errors.email}
                 fullWidth={true}
                 margin={'normal'}
                 label="Email"
                 variant="outlined"
                 placeholder={'Введите ваш email'}
-                {...register('email', {
-                    required: 'Это обязательное поле',
-                })}
+                helperText={errors.email ? `${errors.email.message}` : ''}
+                {...register('email')}
             />
             <TextField
+                error={!!errors.password}
                 type={'password'}
                 fullWidth={true}
                 margin={'normal'}
                 label="Password"
                 variant="outlined"
                 placeholder={'Введите ваш пароль'}
+                helperText={errors.password ? `${errors.password.message}` : ''}
+                {...register('password', {
+                    required: 'Это обязательное поле',
+                })}
             />
             <TextField
+                error={!!errors.confirmPassword}
                 type={'password'}
                 fullWidth={true}
                 margin={'normal'}
                 label="Repeat password"
                 variant="outlined"
                 placeholder={'Повторите ваш пароль'}
+                helperText={errors.confirmPassword ? `${errors.confirmPassword.message}` : ''}
+                {...register('confirmPassword', {
+                    required: 'Это обязательное поле',
+                })}
             />
             <Button
                 type={'submit'}
